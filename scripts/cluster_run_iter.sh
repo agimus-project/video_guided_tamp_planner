bbaea9e47b3d2d9ef9742d48c890abc1e9b8caec
kzorina <kateryna.zorina@cvut.cz>
2025-03-10 16:31:19 +0100

    update cluster batch file

--- scripts/cluster_run_iter.sh
+++ scripts/cluster_run_iter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=python_job_array   # Job name
-#SBATCH --output=job_output_%A_%a.log # Output log file, %A is the job ID and %a is the array task ID
-#SBATCH --error=job_error_%A_%a.log   # Error log file
+#SBATCH --job-name=rrt_connect   # Job name
+#SBATCH --output=logs/job_output_%A_%a.log # Output log file, %A is the job ID and %a is the array task ID
+#SBATCH --error=logs/job_error_%A_%a.log   # Error log file
 #SBATCH --ntasks=1                    # Number of tasks (1 Python process per job)
 #SBATCH --cpus-per-task=4            # Number of cores per task
 #SBATCH --mem=4G                     # Memory per node
@@ -12,7 +12,7 @@ from_iter=$((1000 + (SLURM_ARRAY_TASK_ID - 1) * 5))
 to_iter=$((1000 + SLURM_ARRAY_TASK_ID * 5))
 
 # TODO: modify this for your system
-. /opt/apps/software/Anaconda3/5.0.1/etc/profile.d/conda.sh
+. ~/miniconda3/etc/profile.d/conda.sh
 conda activate gtamp
 export PYTHONPATH=~/video_guided_tamp_planner:~/guided_tamp_benchmark:$PYTHONPATH
 
