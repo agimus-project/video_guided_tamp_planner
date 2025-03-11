10924dfa73395841e77792867d34a6c0b805a44a
kzorina <kateryna.zorina@cvut.cz>
2025-03-11 14:25:16 +0100

    experiment with taking only part of the node

--- scripts/cluster_run_iter.sh
+++ scripts/cluster_run_iter.sh
@@ -6,12 +6,22 @@
 #SBATCH --cpus-per-task=32            # Number of cores per task
 #SBATCH --mem=64G                     # Memory per node
 #SBATCH --time=15:00:00              # Max runtime (1 hour in this example)
-#SBATCH --array=1-5                 # Create a job array with 5 tasks (1-5)
 #SBATCH --nodes=1                     # Request 1 node per task
-#SBATCH --exclusive                   # Request exclusive use of the node
+#SBATCH --array=1-5                  # Job array with 5 tasks
+#SBATCH --nodelist=node-02,node-03,node-04,node-05,node-06  # Specific nodes
 
-from_iter=$((1400 + (SLURM_ARRAY_TASK_ID - 1) * 80))
-to_iter=$((1400 + SLURM_ARRAY_TASK_ID * 80))
+
+# node-01,node-02,node-03,node-04,node-05,node-06,node-07,node-08,node-09,node-10
+# NODES=("node-01" "node-02" "node-03" "node-04" "node-05" "node-06" "node-07" "node-08" "node-09" "node-10")
+NODES=("node-02" "node-03" "node-04" "node-05" "node-06" )
+TASK_INDEX=$((SLURM_ARRAY_TASK_ID - 1))  # Convert SLURM ID (1-based) to 0-based index
+NODE_TO_USE=${NODES[$TASK_INDEX]}        # Select the node for this job
+echo "Running on $NODE_TO_USE"
+export SLURM_NODELIST=$NODE_TO_USE       # Override assigned node
+
+
+from_iter=$((1800 + (SLURM_ARRAY_TASK_ID - 1) * 80))
+to_iter=$((1800 + SLURM_ARRAY_TASK_ID * 80))
 
 # TODO: modify this for your system
 . ~/miniconda3/etc/profile.d/conda.sh
